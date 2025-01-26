890598df8e8645619b4ad9d10a0ca405e06b1549
CGhira03 <cghirardotti9@gmail.com>
2025-01-26 19:18:19 -0300

    new changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 export default defineConfig({
+  base: '/btc-pocket/',
   plugins: [
     vue(),
     vueDevTools(),
+    ghPages(),
   ],
   resolve: {
     alias: {
